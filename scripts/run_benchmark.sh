4461d9191db203e01a932fa097f23172e0d216ca
Alireza Ziabari <alireza.ziabari@gmail.com>
2024-09-07 11:10:56 -0700

    rename python and script file

--- scripts/run_benchmark.sh
+++ scripts/run_benchmark.sh
@@ -3,7 +3,7 @@ cd ..
 
 source ./venv/bin/activate
 
-# LMs=('meta-llama/Meta-Llama-3-8B-Instruct' './experiments/orpo/lora-Meta-Llama-3-8B-Instruct-system2' './experiments/orpo/lora-Meta-Llama-3-8B-Instruct-system1')
+LMs=('meta-llama/Meta-Llama-3-8B-Instruct' './experiments/orpo/lora-Meta-Llama-3-8B-Instruct-system2' './experiments/orpo/lora-Meta-Llama-3-8B-Instruct-system1')
 
 # LMs=('google/gemma-1.1-7b-it' './experiments/orpo/lora-gemma-1.1-7b-it-system2' './experiments/orpo/lora-gemma-1.1-7b-it-system1')
 
@@ -13,10 +13,10 @@ source ./venv/bin/activate
 
 # LMs=('google/gemma-7b' './experiments/orpo/lora-gemma-7b-system2' './experiments/orpo/lora-gemma-7b-system1')
 
-LMs=('./experiments/orpo/lora-Meta-Llama-3-8B-Instruct-system1')
+# LMs=('./experiments/orpo/lora-Meta-Llama-3-8B-Instruct-system1')
 
-gpus=(7)
-dataset='addsub'
+gpus=(0 0 0)
+dataset='coin_flip'
 method='zero_shot'
 batch_size=32
 for lm_index in "${!LMs[@]}"; do
@@ -25,13 +25,14 @@ for lm_index in "${!LMs[@]}"; do
 
     SESSION_NAME="${gpu}_${dataset}"
     echo "${SESSION_NAME}"
-    screen -dmS "$SESSION_NAME" bash -c "
-    CUDA_VISIBLE_DEVICES=$gpu python run_benchmark.py \
+    # screen -dmS "$SESSION_NAME" bash -c "
+    CUDA_VISIBLE_DEVICES=$gpu python benchmark.py \
                                         --limit_dataset_size=0 \
                                         --method=${method} \
                                         --model=${lm} \
                                         --batch_size ${batch_size} \
-                                        --dataset=${dataset} ;
-                                        sleep 15;
-    exit"
+                                        --dataset=${dataset} 
+                                        # ;
+                                        # sleep 15;
+    # exit"
 done 
